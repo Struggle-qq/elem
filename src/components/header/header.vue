64a7cd5e6e031630fa43c294fcc3a98b410c536e
张XX <553688094@qq.com>
2019-01-23 00:52:00 +0800

    初始化文件

---
+++ src/components/header/header.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>header++</div>
+</template>
+<script >
+export default {
+  
+}
+</script>
+
+<style type="text/ecmascript-6" rel="stylesheet/stylus">
+</style>
+
---
+++ src/main.js
@@ -0,0 +1,36 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import VueResource from 'vue-resource';
+import App from './App';
+import goods from 'components/goods/goods.vue';
+import ratings from 'components/ratings/ratings';
+import seller from 'components/seller/seller';
+
+// import 'common/stylus/index.styl';
+
+
+Vue.use(VueRouter);  //使用router路由
+
+
+Vue.use(VueResource);
+
+let app = Vue.extend(App); //使用router路由
+
+let router = new VueRouter({
+	linkActiveClass: 'active'
+});
+
+router.map({     //路径
+	'/goods': {
+		component: goods
+	},
+	'/ratings': {
+		component: ratings
+	},
+	'seller': {
+		component: seller
+	}
+});
+
+router.start(app, '#app'); //调用路由
+router.go('/goods');      //打开页面自动跳指定页面
